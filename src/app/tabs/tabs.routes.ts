97001e21174b29d2d40f09f1c86bfda4edeb327e
Jhoan Carvajal <jhoans127@outlook.es>
2025-09-09 23:53:08 -0500

    feat: remove explore container and associated tabs, update routing to history

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -17,21 +17,6 @@ export const routes: Routes = [
           import('../pages/add-record/add-record.page').then(m => m.AddRecordPage),
       },
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
         path: '',
         redirectTo: 'history',
         pathMatch: 'full',
@@ -40,7 +25,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/history',
     pathMatch: 'full',
   },
 ];
