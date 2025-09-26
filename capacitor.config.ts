0d2cc16db7581b03809da76aac83c792f62b691f
Jhoan Carvajal <jhoans127@outlook.es>
2025-09-26 01:13:47 -0500

    feat: add splash screen functionality and update app icons
    - Integrated @capacitor/splash-screen to show a splash screen for 2 seconds on app launch.
    - Added new icon files in WEBP format for various sizes (48, 72, 96, 128, 192, 256, 512).
    - Created a manifest.webmanifest file to define the app icons for web applications.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'registro-de-ganancias-damian/',
-  webDir: 'www'
+  appId: 'io.ionic.arepasContability',
+  appName: 'ArepasContability',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#cb1111ff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
